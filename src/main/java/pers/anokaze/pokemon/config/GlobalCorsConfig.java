ca40d31becd59cf9e5423ad0a3f4fc35d4b749ac
AnoKaze <422365654@qq.com>
2022-12-19 17:47:48 +0800

    feat: 添加了图片资源，增加了前端请求图片的URL

--- src/main/java/pers/anokaze/pokemon/config/GlobalCorsConfig.java
+++ src/main/java/pers/anokaze/pokemon/config/GlobalCorsConfig.java
@@ -3,6 +3,8 @@ package pers.anokaze.pokemon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -20,6 +22,16 @@ public class GlobalCorsConfig {
                         .allowCredentials(true)
                         .allowedMethods("GET","POST", "PUT", "DELETE")
                         .allowedHeaders("*");
+            }
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer imageResourceConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/image/**").addResourceLocations("classpath:/static/image/");
             }
         };
     }
---
+++ src/main/java/pers/anokaze/pokemon/constant/Type.java
@@ -0,0 +1,22 @@
+package pers.anokaze.pokemon.constant;
+
+public interface Type {
+    int NORMAL = 0;
+    int FIRE = 1;
+    int WATER = 2;
+    int ELECTRIC = 3;
+    int GRASS = 4;
+    int ICE = 5;
+    int FIGHTING = 6;
+    int POISON = 7;
+    int GROUND = 8;
+    int FLYING = 9;
+    int PSYCHIC = 10;
+    int BUG = 11;
+    int ROCK = 12;
+    int GHOST = 13;
+    int DRAGON = 14;
+    int DARK = 15;
+    int STEEL = 16;
+    int FAIRY = 17;
+}
